f3751a06fdaf33546a2c510de4f19353ef20ca14
slotservice <yjin50650@gmail.com>
2026-06-24 18:00:46 +0900

    Chore: update Termii sender ID from N-Alert to naijaride
    Termii rejected 9jaridepro sender (starts with a number). Client registered naijaride as replacement sender ID, pending activation.

--- GoRide_V5.6_Source_Code/Applications/GoRide-5.6/driver/lib/controller/login_controller.dart
+++ GoRide_V5.6_Source_Code/Applications/GoRide-5.6/driver/lib/controller/login_controller.dart
@@ -33,7 +33,7 @@ class LoginController extends GetxController {
           'api_key': _termiiApiKey,
           'message_type': 'NUMERIC',
           'to': fullPhone,
-          'from': 'N-Alert',
+          'from': 'naijaride',
           'channel': 'dnd',
           'pin_attempts': 3,
           'pin_time_to_live': 5,
